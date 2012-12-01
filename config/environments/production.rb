265d6f5982015140f603786b09324df1d9e6be2a
Ryan Heath <rpheath@gmail.com>
2012-11-30 22:57:02 -0500

    log levels

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,6 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+config.logger = Logger.new(STDOUT)
+config.logger.level = Logger::INFO
