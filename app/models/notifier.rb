a0e1c88356e19274a1898a2b9904450022eeef54
Ryan Heath <rpheath@gmail.com>
2014-04-12 00:55:16 -0400

    tweak

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   def subscriber_announcement(email, message)
     recipients email
     from "ritzylunchwv@gmail.com"
-    subject "The Ritzy Announcements"
+    subject "Announcements from The Ritzy!"
     body( :message => message )
   end
 end
